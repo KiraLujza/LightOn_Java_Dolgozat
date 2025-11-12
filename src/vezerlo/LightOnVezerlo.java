573353d76d0ec55119aebdac0369873d8f293d05
Kiss Kira Lujza (Szf_N_2024_2026) <kiss.kira.lujza@diak.szamalk-szalezi.hu>
2025-11-12 14:03:01 +0100

    Alap fajlok letrehozasa

---
+++ src/vezerlo/LightOnVezerlo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vezerlo;
+
+/**
+ *
+ * @author KissKiraLujza(Szf_N_
+ */
+public class LightOnVezerlo {
+    
+}
